ca87d47f6fc119d0be15d57cee9250da29e8f2a0
Dannie Håkansson <dannie.hakansson@cmeducations.se>
2022-06-23 14:30:24 +0200

    Added redirects file

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import { Link } from "react-router-dom";
 import songs from "../data/songs";
 
 function App() {
+  const randomSong = Math.floor(Math.random() * songs.length);
+
   return (
-    <main className="app-main container">
-      {songs.map((song, index) => (
-        <Link key={index} to={`songs/${index}`} className="button">{song.title}</Link>
-      ))}
-    </main>
+    <div className="container">
+      <header className="header padding">Bli onykter nu 😍</header>
+      <main className="app-main">
+        <Link to={`songs/${randomSong}`} className="button primary">
+          Slumpa fram visa 🥂
+        </Link>
+        {songs.map((song, index) => (
+          <Link key={index} to={`songs/${index}`} className="button">
+            {song.title}
+          </Link>
+        ))}
+      </main>
+    </div>
   );
 }
 
